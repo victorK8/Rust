a549d22e12ab89bf84936177846316023023cac9
victorK8 <victormalum@gmail.com>
2021-01-18 16:58:49 +0100

    Example of shadowing

---
+++ shadowing/target/debug/deps/shadowing-16807bb8f671a912.d
@@ -0,0 +1,5 @@
+/home/victor/Rust/shadowing/target/debug/deps/shadowing-16807bb8f671a912: src/main.rs
+
+/home/victor/Rust/shadowing/target/debug/deps/shadowing-16807bb8f671a912.d: src/main.rs
+
+src/main.rs:
---
+++ shadowing/target/debug/shadowing.d
@@ -0,0 +1,1 @@
+/home/victor/Rust/shadowing/target/debug/shadowing: /home/victor/Rust/shadowing/src/main.rs
