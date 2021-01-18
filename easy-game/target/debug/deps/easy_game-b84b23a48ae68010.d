ac5d1dcf8e12d38c4f2c4dc28e8783e5a1973cb0
victorK8 <victormalum@gmail.com>
2021-01-18 16:15:00 +0100

    Easy game example project

---
+++ easy-game/target/debug/deps/easy_game-b84b23a48ae68010.d
@@ -0,0 +1,5 @@
+/home/victor/Rust/easy-game/target/debug/deps/easy_game-b84b23a48ae68010: src/main.rs
+
+/home/victor/Rust/easy-game/target/debug/deps/easy_game-b84b23a48ae68010.d: src/main.rs
+
+src/main.rs:
---
+++ easy-game/target/debug/easy-game.d
@@ -0,0 +1,1 @@
+/home/victor/Rust/easy-game/target/debug/easy-game: /home/victor/Rust/easy-game/src/main.rs
