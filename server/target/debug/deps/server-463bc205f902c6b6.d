de339c5c9c1c4d5f7864de32884ea88e189e205d
victorK8 <victormalum@gmail.com>
2021-01-18 17:36:57 +0100

    Example of little server

---
+++ server/target/debug/deps/server-463bc205f902c6b6.d
@@ -0,0 +1,5 @@
+/home/victor/Rust/server/target/debug/deps/server-463bc205f902c6b6: src/main.rs
+
+/home/victor/Rust/server/target/debug/deps/server-463bc205f902c6b6.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/victor/Rust/server/target/debug/server: /home/victor/Rust/server/src/main.rs
