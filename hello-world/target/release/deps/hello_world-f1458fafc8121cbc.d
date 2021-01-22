e79572371b747a1657306851ecc2b90dd8529c32
victorK8 <victormalum@gmail.com>
2021-01-22 09:49:27 +0100

    hello example builded as release mode

---
+++ hello-world/target/release/deps/hello_world-f1458fafc8121cbc.d
@@ -0,0 +1,5 @@
+/home/victor/Rust/hello-world/target/release/deps/hello_world-f1458fafc8121cbc: src/main.rs
+
+/home/victor/Rust/hello-world/target/release/deps/hello_world-f1458fafc8121cbc.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/release/hello-world.d
@@ -0,0 +1,1 @@
+/home/victor/Rust/hello-world/target/release/hello-world: /home/victor/Rust/hello-world/src/main.rs
